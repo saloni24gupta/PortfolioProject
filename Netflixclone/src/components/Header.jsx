5f1248a2e5214f4dc7132bbd750c15d899bc9f2e
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-06-30 20:59:31 +0530

    settingup signup page

---
+++ Netflixclone/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
+function Header(props) {
+    const navigate = useNavigate();
+  return (
+    <Container className="flex a-center j-between">
+        <div className="logo">
+        <img src={logo} alt="logo" />
+        </div>
+        <button onClick={() => navigate(props.login?"/login": "/signup")}>{props.login ? "Log In": "Sign up"}</button>
+    </Container>
+  )
+}
+
+export default Header
+const Container = styled.div`
+padding: 0 4rem;
+.logo {
+  img {
+    height: 5rem;
+  }
+}
+button {
+    padding: 0.5rem 1rem;
+    background-color: #e50914;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    border-radius: 0.2rem;
+    font-weight: bolder;
+    font-size: 1.05rem;
+
+}
+
+`
--- Netflixclone/src/pages/Signup.jsx
+++ Netflixclone/src/pages/Signup.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import BackgroundImage from '../components/BackgroundImage';
+import styled from 'styled-components';
+import Header from '../components/Header';
 function Signup() {
   return (
-    <div>Signup</div>
+    <Container>
+        <BackgroundImage />
+        <div className="content">
+        <Header/>
+        <div className="body flex column a-center j-center">
+            <div className="text flex column">
+                <h1>Unlimited movies, TV shows and more</h1>
+                <h4>Watch anywhere. Cancel anytime</h4>
+                <h6>Ready to watch  ? Enter your email to create or restart membership</h6>
+
+            </div>
+            <div className="form">
+                <input type="email" placeholder="Email Address" name="email" />
+                <input type="password" placeholder="password" name="password" />
+                <button>Get Started</button>
+            </div>
+            <button>Log In</button>
+            </div>
+        </div>
+    </Container>
   )
 }
 
 export default Signup
+
+const Container = styled.div`
+position: relative;
+.content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+}
+`
