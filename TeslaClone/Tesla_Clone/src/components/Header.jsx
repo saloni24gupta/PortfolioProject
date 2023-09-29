3ddff9fca1cc69805541395fc34b2580b241abde
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-09-29 22:53:57 +0530

    save changes

--- TeslaClone/Tesla_Clone/src/components/Header.jsx
+++ TeslaClone/Tesla_Clone/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons'
 function Header() {
   return (
     <Container>
@@ -8,9 +9,17 @@ function Header() {
       </a>
 
       <Menu>
-
-
+        <a href='#'>Model S</a> 
+        <a href='#'>Model 3</a>
+        <a href='#'>Model X</a>
+        <a href='#'>Model Y</a> 
       </Menu>
+
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        {/* <CustomMenu /> */}
+      </RightMenu>
     </Container>
   )
 }
@@ -20,6 +29,38 @@ export default Header
 const Container = styled.div`
 min-height: 60px;
 position: fixed;
+display: flex;
+align-items: center;
+padding: 0 20px;
+top: 0;
+left: 0;
+right: 0;
 `
 
-const Menu = styled.div``
+const Menu = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex: 1;
+
+
+a {
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 0 10px;
+  flex-wrap: no-wrap;
+}
+`
+
+const RightMenu = styled.div`
+a{
+  font-weight: 600;
+  text-transform: uppercase;
+  flex-wrap: no-warp;
+  margin-right: 10px;
+}
+`
+
+// const CustomMenu = styled(MenuIcon)`
+
+// `
