36c2baf903d461da27325c2fb81f6a3b09e7dab1
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-10-01 23:04:31 +0530

    continue

--- Disney_Clone/src/App.jsx
+++ Disney_Clone/src/App.jsx
@@ -1,13 +1,26 @@
-import react from 'react'
-import Header from './components/Header'
+
 import Home from './components/Home'
 import './App.css'
-
+import { BrowserRouter , Routes, Route, Link } from 'react-router-dom'
+import Detail from './components/Detail'
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
+<Header />
+<BrowserRouter>
+      <Routes>
       <Header />
-      <Home />
+      <S
+        <Route path="/" element={<Home />} >
+      
+          <Route path="delete" element={<Detail/>} />
+   
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+     
     </div>
   )
 }
