4f179dd7c5de844a325f208352c7b41d5a37652b
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-10-02 20:46:41 +0530

    continue

--- Disney_Clone/src/components/Detail.jsx
+++ Disney_Clone/src/components/Detail.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-
+import styled from 'styled-components'
 function Detail() {
   return (
-    <div>
-       <img src="../src/assets/Images/movies.jpg" alt="" />
-    </div>
+    <Container>
+      <Background>
+        <img src="../src/assets/Images/bao-1.jpg" alt="" />
+      </Background>
+      <ImageTitle>
+        <img src="../src/assets/Images/bao-title.png" alt="" />
+      </ImageTitle>
+
+    </Container>
   )
 }
 
 export default Detail
+
+const Container = styled.div`
+min-height: calc(100vh - 70px);
+padding: 0 calc(3.5vw + 5px);
+position: relative;
+
+`
+
+const Background = styled.div`
+positin: fixed;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+z-index: -1;
+opacity: 0.8;
+img {
+  height: 100vh;
+  width: 100vw;
+  object-fit: cover;
+}
+`
+
+
+const ImageTitle = styled.div`
+height: 30vh;
+img {
+  
+}
+`
