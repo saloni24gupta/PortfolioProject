e7191229f18149849df9bd9385f21f0647916c5a
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-10-05 18:28:12 +0530

    concept-practice

--- Disney_Clone/src/App.jsx
+++ Disney_Clone/src/App.jsx
@@ -1,25 +1,17 @@
-import { useState } from "react"
-import {storage} from "./firebase"
-import  {ref} from "firebase/storage"
- // import Home from './components/Home'
-// import './App.css'
-// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-// import Detail from './components/Detail'
-// import Header from './components/Header'
-// import Login from './components/Login'
-function App() {
 
-  const [imageUpload, setImageUpload] = useState(null);
-const uploadImage = () => {
-if(imageUpload == null) return;
-const imageRef = ref(storage, `images/`)
-}
+import { v4 } from "uuid"
+ import Home from './components/Home'
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Detail from './components/Detail'
+import Header from './components/Header'
+import Login from './components/Login'
+function App() {
 return (
     <div className="App">
-      {/* <Header /> */}
-<input type="file" onChange={(event) => {setImageUpload(event.target.files[0])}} />
-<button onClick={uploadImage}> Upload Image</button>
-      {/* <BrowserRouter>
+      <Header />
+
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route exact path="login" element={<Login />} />
@@ -27,7 +19,7 @@ return (
           <Route exact path="detail" element={<Detail />} />
 
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
 
     </div>
   )
